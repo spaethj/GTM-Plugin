8c0b890ac02b2bf6997bd4b6767f6e50e5470968
J.Spaeth <jeremy_350@hotmail.com>
2018-03-05 09:49:57 +0100

    @spaethj, Add the getDataLayer method to bring up in Google Analytics the user ID, the user Email and witch posts he consults.

--- google-tag-manager-plugin.php
+++ google-tag-manager-plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Google Tag Manager Plugin
 Plugin URI:
 Description: Add all Google Tag Manager scripts in Wordpress websites.
-Version: 1.0
+Version: 1.1
 Author: Jeremy Spaeth
 Author URI:
 License: GPL v3
@@ -13,8 +13,36 @@ class GoogleTagManager
 {
 	public function __construct()
 	{
+		add_filter('wp_head', array($this, 'getDataLayer'), 50, 0);
 		add_filter('wp_head', array($this, 'GtmHead'), 51, 0);
 		add_filter('wp_footer', array($this, 'GtmBody'));
+	}
+
+	function getDataLayer()
+	{
+		// Get current user informations if is logged in.
+		$user = wp_get_current_user();
+
+		// Return true if is single post.
+		$is_post = 'false';
+		if (is_single()) {
+			$is_post = 'true';
+		}
+
+		echo '
+<script>
+    var dataLayer = [{
+        "user": {
+            "userId" : "' . $user->ID. '",
+            "userEmail" : "' . $user->user_email . '",
+            "userEmailMd5" : "' . md5($user->user_email) . '"
+        },
+        "site": {
+            "is_post" : "' . $is_post . '"
+        }
+    }];
+</script>
+            ';
 	}
 
 	function GtmHead()
@@ -25,7 +53,7 @@ class GoogleTagManager
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-XXXXXXX');
+})(window,document,'script','dataLayer','GTM-KBF7DJ2');
 </script>
 <!-- End Google Tag Manager -->
 ";
@@ -36,7 +64,7 @@ class GoogleTagManager
 		echo '
 <!-- Google Tag Manager (noscript) -->
 <noscript>
-    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX" height="0" width="0" style="display:none; visibility:hidden"></iframe>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KBF7DJ2" height="0" width="0" style="display:none; visibility:hidden"></iframe>
 </noscript>
 <!-- End Google Tag Manager (noscript) -->
         ';
